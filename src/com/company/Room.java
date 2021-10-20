db2af7ed67697584cb2feff4d6efabd06a64b42f
niculaec <niculaec.alexandru@gmail.com>
2021-10-20 10:27:41 +0100

    added Room class
    define a room

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,4 +1,25 @@
 package com.company;
 
 public class Room {
+    int roomNumber;
+    double price;
+    Guest guest;
+
+
+    public Room(int roomNumber, double price,Guest guest) {
+        this.roomNumber = roomNumber;
+        this.price = price;
+        this.guest = guest;
+    }
+    public Room checkIn(Guest guest){
+
+        return checkIn(guest);
+    }
+    public Room checkOut(Guest guest){
+
+        return checkOut(guest);
+    }
+
+
+
 }
