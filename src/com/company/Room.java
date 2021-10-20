d8f45f6d8351fd0c67b88993f2be8be81da1a6f8
niculaec <niculaec.alexandru@gmail.com>
2021-10-21 00:40:32 +0100

    Room class checkIn and CheckOut method and RoomNumber enum class

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -5,21 +5,28 @@ public class Room {
     double price;
     Guest guest;
 
-
-    public Room(int roomNumber, double price,Guest guest) {
+    public Room(int roomNumber, double price) {
         this.roomNumber = roomNumber;
         this.price = price;
+    }
+
+    public void checkIn(Guest guest){
         this.guest = guest;
     }
-    public Room checkIn(Guest guest){
 
-        return checkIn(guest);
-    }
-    public Room checkOut(Guest guest){
-
-        return checkOut(guest);
+    public void checkOut(Guest guest){
+        this.guest = null;
     }
 
-
+    public enum RoomNumber{
+        ROOM_NUMBER101,
+        ROOM_NUMBER201,
+        ROOM_NUMBER301,
+        ROOM_NUMBER401,
+        ROOM_NUMBER501,
+        ROOM_NUMBER601,
+        ROOM_NUMBER701,
+        ROOM_NUMBER801;
+    }
 
 }
