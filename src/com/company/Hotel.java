4b98143e417fa5fea3c01ae9be7a1175272dfe37
niculaec <niculaec.alexandru@gmail.com>
2021-10-20 10:28:57 +0100

    Hotel class minor chages

--- src/com/company/Hotel.java
+++ src/com/company/Hotel.java
@@ -1,3 +1,24 @@
-package com.company;public class Hotel {
+package com.company;
+
+import java.util.ArrayList;
+
+public class Hotel {
+    Hotel hotelManager = new Hotel();
+    ArrayList<Room> roomLists = new ArrayList<Room>();
+    ArrayList<Guest> guestLists = new ArrayList<Guest>();
+
+    public void saveGuest(Guest guest) {
+        guestLists.add(guest);
+    }
+
+    public void checkIn(Guest guest){
+
+
+    }
+
     public void checkOut( Guest guest){
+
+    }
+
 }
+
