5ea32eaf0b00d5ec438756ae3287816781344164
niculaec <niculaec.alexandru@gmail.com>
2021-10-20 10:28:19 +0100

    This is Main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Guest guest1 = new Guest("Alex", 33.00);
+        Guest guest2 = new Guest("Sergiu", 150.00);
+        Guest guest3 = new Guest("Roxana", 600.00);
+        Guest guest4 = new Guest("Andreea", 220.00);
+        Guest guest5 = new Guest("Radu", 777.00);
+
+        //Hotel.hotelManager.saveGuest(guest1);
+
+        System.out.println(guest1);
+        ArrayList<Hotel> resultList = Hotel.class.getMethods();
+        System.out.println(resultList);
+
     }
 }
