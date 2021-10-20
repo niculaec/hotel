bfa77cc5bff9f3e65f72031303980d4660a224da
niculaec <niculaec.alexandru@gmail.com>
2021-10-20 10:25:59 +0100

    Created class Guest
    this class holds parameters and getName(), getMoney() methods

--- src/com/company/Guest.java
+++ src/com/company/Guest.java
@@ -1,4 +1,33 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Guest {
+    String name;
+    Double money;
+
+    public Guest(String name, Double money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public Guest() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{" +
+                "name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
 }
