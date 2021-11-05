ab32f2285d959411e142cb27e1354f150cc96833
niculaec <niculaec.alexandru@gmail.com>
2021-11-05 12:07:46 +0000

    I added a new hotel
    hotel athenePalace was added and the cliests was checked IN and then checked OUT, last 2 lines of code are ideeas to improve the project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,30 @@
 package com.company;
 
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Guest guest1 = new Guest("Alex", 33.00);
-        Guest guest2 = new Guest("Sergiu", 150.00);
-        Guest guest3 = new Guest("Roxana", 600.00);
-        Guest guest4 = new Guest("Andreea", 220.00);
-        Guest guest5 = new Guest("Radu", 777.00);
+        Hotel athenePalace = new Hotel(272);
 
-        Hotel hotel = new Hotel();
+        athenePalace.checkIn(new Guest("Alex", 9.00));
+        athenePalace.checkIn(new Guest("Sergiu", 150.00));
+        athenePalace.checkIn(new Guest("Roxana", 600.00));
+        athenePalace.checkIn(new Guest("Andreea", 220.00));
+        athenePalace.checkIn(new Guest("Radu", 777.00));
 
-        hotel.checkIn(guest1);
-        hotel.checkIn(guest2);
-        hotel.checkIn(guest3);
-        hotel.checkIn(guest4);
-        hotel.checkIn(guest5);
+        System.out.println("This is the Room List:");
+            ArrayList<Guest> guestList = athenePalace.getAllClients();
+            athenePalace.checkOut(guestList.get(2));
+            athenePalace.checkOut(guestList.get(3));
+            athenePalace.checkOut(guestList.get(0));
+            athenePalace.checkOut(guestList.get(0));
+            athenePalace.checkOut(guestList.get(0));
 
-        System.out.println(hotel.roomLists);
-        System.out.println("Random price for a room is :");
+        athenePalace.displayHotelRevenue();
 
-        System.out.println(Room.generateRoomPriceInRange(10,100) + "$");
-
-        System.out.println(Room.generateRoomPriceInRange2(6,123) + "$");
-        
-        System.out.println("Hotel rooms are:");
-        for (Room ddd: Room.generateRooms(5)) {
-            System.out.println(ddd.toString());
-        }
-
-    }
+        //sa introduc numarul camere si nu guestList.indexOf
+        //sa recazam acelasi client dar cand se intoarce sa vina cu banii cu care a plecat
+   }
 }
