b0a9e10150290b8700cc5924ee10662fecd6e6d2
niculaec <niculaec.alexandru@gmail.com>
2021-11-05 12:12:56 +0000

    Multiple method was added, see description
    getAllClients() where we get all clients from guestList;
    checkIn(Guest guest) where we sort our guestList in reverse order to get the higher price of room and then take the money from the customer account  wich will bee added to hotel revenue;
    checkOut( Guest guest) where a guest is schecked out from the assgned room and the room became free.

--- src/com/company/Hotel.java
+++ src/com/company/Hotel.java
@@ -1,17 +1,68 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Hotel {
-    ArrayList<Room> roomLists = new ArrayList<Room>();
+    ArrayList<Room> roomList = new ArrayList<Room>();
+    double revenue;
 
-    public void checkIn(Guest guest){
+    public Hotel(int numberOfRooms) {
 
-       // roomLists.add(guest);
+        roomList.addAll(Room.generateRooms(numberOfRooms));
     }
 
-    public void checkOut( Guest guest){
+    public void displayHotelRevenue(){
+        System.out.println("This is Hotel revenue:" + "\n" + revenue + "$");
+    }
 
+    public ArrayList<Guest> getAllClients(){
+        ArrayList<Guest> guestsList = new ArrayList<Guest>();
+        for (Room room:roomList) {
+            if (room.guest != null){
+                guestsList.add(room.guest);
+            }
+        }
+        return guestsList;
+    }
+
+    // luam un client ii atribuim cea mai scumpa camera pe care si-o permite in functie de banii atribuiti, ii scadem din banii clientului suma respectiva si o adaugam la venitul(revenue) Hotelului.
+
+    //indiciu: sortam lista de camere dupa pret descrescator. Pentru fiecare camera comparam pretul camerei cu banii clientului. Bagam clientul in camera(room check in), scadem pretul camerei din banii clientului si ii adaugam la venitul hotelului.
+
+    /**
+     *
+     * @param guest
+     */
+    public void checkIn(Guest guest){
+        boolean guestWasCheckIn = false;
+        roomList.sort(Collections.reverseOrder(Room::compareTo));
+        for (Room room:roomList) {
+            if (room.guest == null && room.price <= guest.money){
+                guest.money = guest.money - room.price;
+                revenue = revenue + room.price;
+                room.checkIn(guest);
+                guestWasCheckIn = true;
+                break;
+            }
+        }
+        if (!guestWasCheckIn){
+            System.out.println("Not enough money");
+        }
+    }
+   //decazam clientul din camera unde este cazat(de unde stim noi asta?)
+    //pentru fiecare guest care nu este null
+    public void checkOut( Guest guest){
+        for (Room room:roomList) {
+            if (room.guest == guest){
+                room.checkOut(guest);
+                break;
+            }
+        }
+
+       // guest
     }
 
 }
